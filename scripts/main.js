44de00f33bd2ac3ad5728913091c8126c5863961
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-12-01 14:09:58 +0100

    Añadimos nuevas funcionalidades

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,21 @@ vehiculo1.arrancar();
 vehiculo1.acelerar(160);
 vehiculo1.frenar();
 
+// variable tipo object de un objeto vehiculo
+const vehiculoObject = {
+    marca: 'BMW',
+    modelo: 'M2 Competition',
+    color: 'rojo',
+    fabricacion: 2021,
+    cilindrada: '411'
+}
+
+//almacenar datos: guardar un JSON en el LocalStorage
+localStorage.setItem("Vehículo", JSON.stringify(vehiculoObject));
+
+//recuperar datos: mostrar el object en consola del JSON que hemos guardado en el LocalStorage
+console.log(localStorage.getItem("Vehiculo"));
+
 //muestra un objeto furgoneta
 const furgoneta1 = new Furgoneta('Citroën', 'C15', 'blanca', 1995, '120', 10);
 mostrarResultado(`<br/> <br/>-- Furgoneta 1--`);
@@ -29,6 +44,9 @@ furgoneta1.mostrarDatos();
 furgoneta1.arrancar();
 furgoneta1.acelerar(210);
 furgoneta1.frenar();
+
+//Guardar en el LocalStorage la marca de la furgoneta
+localStorage.setItem("marca furgoneta", furgoneta1.marca);
 
 //muestra un objeto todoterreno
 const todoterreno1 = new Todoterreno('Mitsubishi ', 'Pajero', 'gris', 2015, '190', 4);
@@ -38,3 +56,26 @@ mostrarResultado(`<br/>Tiene tracción ` + todoterreno1.traccion);
 todoterreno1.arrancar();
 todoterreno1.acelerar(100);
 todoterreno1.frenar();
+
+//generar variable de tipo object con POO
+const todoterrenoObject = {
+    marca: todoterreno1.marca,
+    modelo: todoterreno1.modelo,
+    color: todoterreno1.color,
+    fabricacion: todoterreno1.fabricacion,
+    cilindrada: todoterreno1.cilindrada,
+    traccion: todoterreno1.traccion
+}
+
+//almacenar los datos en LocalStorage mediante un bucle iterativo
+for(let key in todoterrenoObject){
+    if(todoterrenoObject.hasOwnProperty(key)){
+        localStorage.setItem(key, todoterrenoObject[key]);
+    }
+}
+
+//eliminar un elemento del localstorage
+localStorage.removeItem('color');
+
+//eliminar todos los elementos del localstorage
+//localStorage.clear();
